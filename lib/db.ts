09500139744aa6e53270a0e959560ef3bcfe8898
Alex <87208475+K0uzer@users.noreply.github.com>
2024-10-01 15:28:44 +0300

    refactor: Fixed errors

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
 declare global {
-    var prisma: PrismaClient | undefined
+    let prisma: PrismaClient | undefined
 }
 
-export const db = globalThis.prisma || new PrismaClient()
+export const db = PrismaClient.globalThis.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
-    globalThis.prisma = db
+    PrismaClient.globalThis.prisma = db
 }
