eba8fe2824a146ad6b0e30d8b32d41af7abc06fc
Alex <87208475+K0uzer@users.noreply.github.com>
2024-10-06 22:37:25 +0300

    feat: Add JWT, page settings, routes for auth and other path

--- actions/register.ts
+++ actions/register.ts
@@ -1,13 +1,34 @@
 'use server'
 
-import { z } from 'zod'
+import { db } from '@lib/db'
+import bcrypt from 'bcrypt'
+import * as z from 'zod'
 
 import { RegisterSchema } from '@schemas/index'
+import { getUserByEmail } from 'data/user'
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
     const validatedFields = RegisterSchema.safeParse(values)
 
     if (!validatedFields.success) return { error: 'Invalid fields!' }
 
-    return { success: 'Email sent!' }
+    const { email, password, name } = validatedFields.data
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const existingUser = await getUserByEmail(email)
+
+    if (existingUser) {
+        return { error: 'Email already in use!' }
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        },
+    })
+
+    return { success: 'User created!' }
 }
--- lib/db.ts
+++ lib/db.ts
@@ -4,8 +4,8 @@ declare global {
     let prisma: PrismaClient | undefined
 }
 
-export const db = PrismaClient.globalThis.prisma || new PrismaClient()
+export const db = globalThis.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') {
-    PrismaClient.globalThis.prisma = db
+    globalThis.prisma = db
 }
